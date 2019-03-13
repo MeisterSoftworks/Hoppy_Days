79461f2b85c6f35b8b8802237e7cd1e26a42178f
1ntrepid MAXIMUM+ <blackmathsavior@gmail.com>
2019-03-13 13:55:48 -0700

    [3-21] AnimatedSprite - Making Bunny Run

--- Hoppy Days/Scripts/Player.gd
+++ Hoppy Days/Scripts/Player.gd
@@ -3,14 +3,18 @@ extends KinematicBody2D
 const SPEED = 750
 var motion = Vector2()
 
-func get_input():
-	motion = Vector2()
-	if Input.is_action_pressed("ui_right"):
-		motion.x += 1
-	if Input.is_action_pressed("ui_left"):
-		motion.x -= 1
-	motion = motion * SPEED
-
 func _physics_process(delta):
-	get_input()
+	if Input.is_action_pressed("ui_right") && !Input.is_action_pressed("ui_left"):
+		motion.x = SPEED
+		$AnimatedSprite.play("run")
+		
+	elif Input.is_action_pressed("ui_left") && !Input.is_action_pressed("ui_right"):
+		motion.x = -SPEED
+		$AnimatedSprite.play("run")
+	else:
+		motion.x = 0
+		$AnimatedSprite.play("idle")
+		
+	if motion.x:
+		$AnimatedSprite.flip_h = motion.x < 0
 	move_and_slide(motion)
