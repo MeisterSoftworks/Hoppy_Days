20eac7bfc477baba07ff559164eff5b12a52d56d
1ntrepid MAXIMUM+ <blackmathsavior@gmail.com>
2019-03-14 13:24:34 -0700

    [3-23] Cleaning Up The Player's Code

--- Hoppy Days/Scripts/Player.gd
+++ Hoppy Days/Scripts/Player.gd
@@ -1,40 +1,46 @@
 extends KinematicBody2D
 
-const SPEED = 750
+const MOVE_SPD = 750
 const GRAVITY = 2800
 const UP = Vector2(0,-1)
 const JUMP_SPD = 1400
 
 var motion = Vector2()
 
-func _physics_process(delta):
-	get_fall(delta)
-	get_move()
-	get_jump()
-	motion = move_and_slide(motion, UP)
+func _process(delta):
+	update_animation(motion)
 
-func get_fall(delta):
-	#Accelerates player downward until landing on ground
-	if is_on_floor() && $RayCast2D.is_colliding():
+func _physics_process(delta):
+	update_motion(delta)
+
+func update_motion(delta):
+	grav_ctrl(delta)
+	move_ground()
+	motion = move_and_slide(motion, UP) #Stops "motion.y" buildup
+
+func update_animation(motion):
+	$AnimatedSprite.update(motion)
+
+func grav_ctrl(delta):
+	#GRAVITY
+	if is_on_floor() && $RayCast2D.is_colliding(): #Accelerate player downward until landed on floor
 		motion.y = 0
 	else:
 		motion.y += GRAVITY * delta
 
-func get_move():
-	if Input.is_action_pressed("ui_right") && !Input.is_action_pressed("ui_left"):
-		motion.x = SPEED
-		$AnimatedSprite.play("run")
-		
-	elif Input.is_action_pressed("ui_left") && !Input.is_action_pressed("ui_right"):
-		motion.x = -SPEED
-		$AnimatedSprite.play("run")
+func move_ground():
+	var move_right = Input.is_action_pressed("ui_right")
+	var move_left = Input.is_action_pressed("ui_left")
+	var jump = Input.is_action_just_pressed("ui_up")
+	
+	if move_right && !move_left:
+		motion.x = MOVE_SPD
+	elif move_left && !move_right:
+		motion.x = -MOVE_SPD
 	else:
 		motion.x = 0
-		$AnimatedSprite.play("idle")
-		
-	if motion.x: #If there is an X value in Vector2, call "motion"
-		$AnimatedSprite.flip_h = motion.x < 0 #Set flip_h's true/false value to be same as motion.x < 0
-
-func get_jump():
-	if is_on_floor() && Input.is_action_pressed("ui_up"):
+	
+		#JUMP
+	if jump && is_on_floor():
 		motion.y = -JUMP_SPD
+
---
+++ Hoppy Days/Scripts/PlayerAnimation.gd
@@ -0,0 +1,14 @@
+extends AnimatedSprite
+
+func update(motion):
+	if motion.y < 0:
+		play("jump")
+	elif motion.x > 0:
+		play("run")
+	elif motion.x < 0:
+		play("run")
+	else:
+		play("idle")
+	
+	if motion.x: #If there is an X value in Vector2, call "motion"
+		flip_h = motion.x < 0 #Set flip_h's true/false value to be same as motion.x < 0
