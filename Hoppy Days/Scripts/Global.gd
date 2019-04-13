3c2969b28af00acb040c500ea959b77fddf4cb00
1ntrepid MAXIMUM+ <blackmathsavior@gmail.com>
2019-04-13 09:33:09 -0700

    [3-38] Enter the Spikeman

--- Hoppy Days/Scripts/Global.gd
+++ Hoppy Days/Scripts/Global.gd
@@ -1,8 +1,13 @@
 extends Node
 
+# Nodes - set automatically
 var GameState
 var Player
 var GUI
 var SFX_Jump
 var SFX_Pain
 var SFX_extraLife
+
+# Scene Locations - Must be set MANUALLY
+var Level_1 = "res://Scenes/Levels/Level_1.tscn"
+var GameOver = "res://Scenes/Levels/GameOver.tscn"
